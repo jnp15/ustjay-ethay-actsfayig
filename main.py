bcf9642e35079b4205a691601da6eb10521b5bca
Jonathan Patten <jnp15@hotmail.com>
2020-05-03 12:10:59 -0700

    added url reference

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
-def get_fact():
+# template = """
+# {instance
 
+def get_fact():
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -17,12 +19,39 @@ def get_fact():
     return facts[0].getText()
 
 
+def get_pig_latin(quote):
+
+    payload = {'input_text': quote}
+
+    response = requests.post("http://hidden-journey-62459.herokuapp.com/piglatinize/", data = payload)
+    #response = requests.get("http://hidden-journey-62459.herokuapp.com", params = payload)
+
+    #response = requests.get('http://hidden-journey-62459.herokuapp.com/get', params = payload)
+    #response = requests.post('http://hidden-journey-62459.herokuapp.com/post', params=payload)
+    print('getpiglattenresponse: ', response.url)
+    return response.url
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    print('fact',fact)
+    header = get_pig_latin(fact)
+    print('header',header)
+    print('test ', Response(response=header, mimetype='gzip'))
+    return Response(response=header, mimetype='gzip')
+    # return
 
 
+'''
+Request URL https://hidden-journey-62459.herokuapp.com/piglatinize/
+Request Method: POST
+input_text: Two paths diverge
+https://hidden-journey-62459.herokuapp.com/
+Location: http://hidden-journey-62459.herokuapp.com/esultray/a4de5f6cbef49165d0cc5d49b4ae4b4b/
+http://hidden-journey-62459.herokuapp.com/esultray/a4de5f6cbef49165d0cc5d49b4ae4b4b/
+
+'''
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
