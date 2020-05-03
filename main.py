c1a67309fd4b61cce6d98e92d65bdaaacceaf71d
Jonathan Patten <jnp15@hotmail.com>
2020-05-03 13:17:10 -0700

    removed extra comments and print messages

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import os
-
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
-
-# template = """
-# {instance
 
 def get_fact():
     response = requests.get("http://unkno.com")
@@ -24,11 +19,6 @@ def get_pig_latin(quote):
     payload = {'input_text': quote}
 
     response = requests.post("http://hidden-journey-62459.herokuapp.com/piglatinize/", data = payload)
-    #response = requests.get("http://hidden-journey-62459.herokuapp.com", params = payload)
-
-    #response = requests.get('http://hidden-journey-62459.herokuapp.com/get', params = payload)
-    #response = requests.post('http://hidden-journey-62459.herokuapp.com/post', params=payload)
-    print('getpiglattenresponse: ', response.url)
     return response.url
 
 
@@ -37,21 +27,8 @@ def home():
     fact = get_fact().strip()
     print('fact',fact)
     header = get_pig_latin(fact)
-    print('header',header)
-    print('test ', Response(response=header, mimetype='gzip'))
     return Response(response=header, mimetype='gzip')
-    # return
 
-
-'''
-Request URL https://hidden-journey-62459.herokuapp.com/piglatinize/
-Request Method: POST
-input_text: Two paths diverge
-https://hidden-journey-62459.herokuapp.com/
-Location: http://hidden-journey-62459.herokuapp.com/esultray/a4de5f6cbef49165d0cc5d49b4ae4b4b/
-http://hidden-journey-62459.herokuapp.com/esultray/a4de5f6cbef49165d0cc5d49b4ae4b4b/
-
-'''
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
